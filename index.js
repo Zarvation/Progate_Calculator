01e7e8ebf45d342dac151ce6a0140fbcf1dee4a8
zarvation <aerotroyard@gmail.com>
2021-11-11 21:33:52 +0700

    Add Javascript till number display

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-alert('alert dari javascript');
+console.log("Test");
+const numbers=document.querySelectorAll('.num');
+const calcscreen=document.querySelector('.calc-screen');
+const clear=document.querySelector('btn-AC');
+
+
+const updatescreen=(number)=>{
+    calcscreen.value=number;
+}
+
+numbers.forEach((number)=>{
+    number.addEventListener('click',(event)=>{
+        inputNumber(event.target.value);
+        updatescreen(curNumber);
+    })
+})
+
+let prevNumber='';
+let curNumber='0';
+let operator='';
+
+const inputNumber=(num)=>{
+    if(curNumber==='0'){
+        curNumber=num;
+    }
+    else{
+        curNumber+=num;
+    }
+}
+
+const operator = document.querySelectorAll('operator');
+
+operator.forEach=((op)=>{
+    operator.addEventListener('click',(event)=>{
+        console.log(event.target.value);
+    })
+})

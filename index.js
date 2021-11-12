ac30621f58b996e269af343c154924db08cc1782
zarvation <aerotroyard@gmail.com>
2021-11-12 23:04:13 +0700

    Update HTML Finish Sesuai Modul

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 console.log("Test");
-const numbers=document.querySelectorAll('.num');
+const numbers=document.querySelectorAll('.numbers');
 const calcscreen=document.querySelector('.calc-screen');
-const clear=document.querySelector('btn-AC');
+const operators = document.querySelectorAll('.operator');
+const clear=document.querySelector('.btn-AC');
+const equal = document.querySelector('.btn-equal');
+const decimal=document.querySelector('.btn-decimals');
+let prevNumber='';
+let curNumber=`${calcscreen.value}`;
+let operator='';
 
-
-const updatescreen=(number)=>{
+const updateScreen=(number)=>{
     calcscreen.value=number;
 }
-
-numbers.forEach((number)=>{
-    number.addEventListener('click',(event)=>{
-        inputNumber(event.target.value);
-        updatescreen(curNumber);
-    })
-})
-
-let prevNumber='';
-let curNumber='0';
-let operator='';
 
 const inputNumber=(num)=>{
     if(curNumber==='0'){
@@ -28,10 +22,79 @@ const inputNumber=(num)=>{
     }
 }
 
-const operator = document.querySelectorAll('operator');
-
-operator.forEach=((op)=>{
-    operator.addEventListener('click',(event)=>{
-        console.log(event.target.value);
+numbers.forEach((number)=>{
+    number.addEventListener('click',(event)=>{
+        inputNumber(event.target.value);
+        updateScreen(curNumber);
     })
+})
+
+const inputOp=(op)=>{
+    if (operator===''){
+        prevNumber=curNumber;
+    }
+    operator=op;
+    curNumber+=op;
+    updateScreen(curNumber);
+}
+
+operators.forEach((op)=>{
+    op.addEventListener('click',(event)=>{
+        inputOp(event.target.value);
+    })
+})
+
+const calculate=()=>{
+    let result='';
+    switch(operator){
+        case '+':
+            result=parseFloat(prevNumber)+parseFloat(curNumber);
+            console.log(result);
+            break
+        case '-':
+            result=parseFloat(prevNumber)-parseFloat(curNumber);
+            console.log(result);
+            break
+        case '/':
+            result=parseFloat(prevNumber)/parseFloat(curNumber);
+            console.log(result);
+            break
+        case '*':
+            result=parseFloat(prevNumber)*parseFloat(curNumber);
+            console.log(result);
+            break
+        case '%':
+            result=parseFloat(prevNumber/100);
+            console.log(result);
+            break
+        default:
+            break;
+    }
+    curNumber=result;
+    operator='';
+}
+
+equal.addEventListener('click',()=>{
+    calculate();
+    updateScreen(curNumber);
+})
+
+const clearAll=()=>{
+    prevNumber='';
+    operator='';
+    curNumber='0';
+}
+
+clear.addEventListener('click',()=>{
+    clearAll();
+    updateScreen(curNumber);
+})
+
+const inputDecimal=(dot)=>{
+    curNumber+=dot;
+}
+
+decimal.addEventListener('click',(event)=>{
+    inputDecimal(event.target.value);
+    updateScreen(curNumber);
 })
